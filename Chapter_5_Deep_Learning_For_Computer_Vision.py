93ac26832be3be03dc26c4f02e04bd0ed4df099b
owatkins <oisin.watkins@analog.com>
2020-05-18 12:29:13 +0100

    Completed the vast majority of the Cats vs. Dogs examples.

--- Chapter_5_Deep_Learning_For_Computer_Vision.py
+++ Chapter_5_Deep_Learning_For_Computer_Vision.py
@@ -10,10 +10,12 @@ This chapter covers:
 
 import os
 import shutil
+import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.applications import VGG16
 from tensorflow.keras import models, layers, optimizers
 
 if __name__ == '__main__':
@@ -202,4 +204,140 @@ if __name__ == '__main__':
 
             plt.show()
 
-    cats_vs_dogs()
+        def train_model_with_augmentation():
+            model = models.Sequential()
+            model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+            model.add(layers.MaxPooling2D((2, 2)))
+            model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+            model.add(layers.MaxPooling2D((2, 2)))
+            model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+            model.add(layers.MaxPooling2D((2, 2)))
+            model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+            model.add(layers.MaxPooling2D((2, 2)))
+            model.add(layers.Flatten())
+            model.add(layers.Dropout(0.5))
+            model.add(layers.Dense(512, activation='relu'))
+            model.add(layers.Dense(1, activation='sigmoid'))
+
+            model.summary()
+            model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+
+            train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                               rotation_range=40,
+                                               width_shift_range=0.2,
+                                               height_shift_range=0.2,
+                                               shear_range=0.2,
+                                               zoom_range=0.2,
+                                               horizontal_flip=True)
+
+            test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+            train_generator = train_datagen.flow_from_directory(train_dir,
+                                                                target_size=(150, 150),
+                                                                batch_size=20,
+                                                                class_mode='binary')
+            validation_generator = test_datagen.flow_from_directory(validation_dir,
+                                                                    target_size=(150, 150),
+                                                                    batch_size=20,
+                                                                    class_mode='binary')
+
+            history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=100,
+                                          validation_data=validation_generator, validation_steps=50)
+
+            model.save(filepath='C:\\Datasets\\dogs-vs-cats\\cats_and_dogs_small\\cats_and_dogs_small_2.h5')
+
+            acc = history.history['acc']
+            val_acc = history.history['val_acc']
+            loss = history.history['loss']
+            val_loss = history.history['val_loss']
+
+            epochs = range(1, len(acc) + 1)
+
+            plt.plot(epochs, acc, 'bo', label='Training acc')
+            plt.plot(epochs, val_acc, 'b', label='Validation acc')
+            plt.title('Training and Validation Accuracy vs. Epoch #')
+            plt.legend()
+
+            plt.figure()
+
+            plt.plot(epochs, loss, 'bo', label='Training loss')
+            plt.plot(epochs, val_loss, 'b', label='Validation loss')
+            plt.title('Training and Validation Loss vs. Epoch #')
+            plt.legend()
+
+            plt.show()
+
+        def reuse_conv_base():
+            """
+            This function will reuse the bottom of the VGG16 network to extract the features of the images and then use
+            a new classifier to perform the classification. The issue with this approach is that the classifier overfits
+            almost immediately. The chapter in the book goes into detail into how this can be avoided by incorporating
+            the base into the model and training end - to - end with augmentation, however this is too expensive to run
+            on CPU and is essentially a mix of train_model_with_augmentation() and reuse_conv_base()
+            """
+            conv_base = VGG16(weights='imagenet', include_top=False, input_shape=(150, 150, 3))
+            conv_base.summary()
+
+            datagen = ImageDataGenerator(rescale=1./255)
+            batch_size = 20
+
+            def extract_features(directory, sample_count):
+                features = np.zeros(shape=(sample_count, 4, 4, 512))
+                labels = np.zeros(shape=(sample_count))
+
+                generator = datagen.flow_from_directory(directory,
+                                                        target_size=(150, 150),
+                                                        batch_size=batch_size,
+                                                        class_mode='binary')
+
+                i = 0
+                for inputs_batch, labels_batch in generator:
+                    features_batch = conv_base.predict(inputs_batch)
+                    features[i * batch_size: (i + 1) * batch_size] = features_batch
+                    labels[i * batch_size: (i + 1) * batch_size] = labels_batch
+
+                    i += 1
+                    if i * batch_size >= sample_count:
+                        break
+                return features, labels
+
+            train_features, train_labels = extract_features(train_dir, 2000)
+            validation_features, validation_labels = extract_features(validation_dir, 1000)
+            test_features, test_labels = extract_features(test_dir, 1000)
+
+            train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+            validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+            test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
+
+            model = models.Sequential()
+            model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+            model.add(layers.Dropout(0.5))
+            model.add(layers.Dense(1, activation='sigmoid'))
+
+            model.compile(optimizer=optimizers.RMSprop(lr=2e-5), loss='binary_crossentropy', metrics=['acc'])
+
+            history = model.fit(train_features, train_labels, epochs=30, batch_size=20,
+                                validation_data=(validation_features, validation_labels))
+
+            model.save(filepath='C:\\Datasets\\dogs-vs-cats\\cats_and_dogs_small\\cats_and_dogs_small_ReuseBottom.h5')
+
+            acc = history.history['acc']
+            val_acc = history.history['val_acc']
+            loss = history.history['loss']
+            val_loss = history.history['val_loss']
+
+            epochs = range(1, len(acc) + 1)
+
+            plt.plot(epochs, acc, 'bo', label='Training acc')
+            plt.plot(epochs, val_acc, 'b', label='Validation acc')
+            plt.title('Training and Validation Accuracy vs. Epoch #')
+            plt.legend()
+
+            plt.figure()
+
+            plt.plot(epochs, loss, 'bo', label='Training loss')
+            plt.plot(epochs, val_loss, 'b', label='Validation loss')
+            plt.title('Training and Validation Loss vs. Epoch #')
+            plt.legend()
+
+            plt.show()
